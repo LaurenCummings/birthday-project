039b99896594fe4accdba34188de4e451cd24cf3
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-29 01:17:28 -0400

    Added styling to container

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [people, setPeople] = useState(data);
 
   return (
-    <main>
+    <main className="app">
       <section className="container">
         <h3>{people.length} birthdays today</h3>
         <List people={people} />
